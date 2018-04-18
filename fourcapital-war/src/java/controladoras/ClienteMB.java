a13f820915df75a93a33474b17d3374078870db1
dcusoft <38126677+dcusoft@users.noreply.github.com>
2018-04-18 18:00:21 -0500

    conexion a servicio

--- fourcapital-war/src/java/controladoras/ClienteMB.java
+++ fourcapital-war/src/java/controladoras/ClienteMB.java
@@ -5,9 +5,20 @@
  */
 package controladoras;
 
+import clienteRest.ClienteREST;
+import com.google.gson.reflect.TypeToken;
+import conexion.ConexionHTTP;
+import entities.Cliente;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import static javafx.scene.input.KeyCode.T;
+import javax.faces.event.ActionEvent;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.GenericType;
 
 /**
  *
@@ -23,4 +34,20 @@ public class ClienteMB implements Serializable {
     public ClienteMB() {
     }
     
+   public void listcliente(ActionEvent actionEvent){
+       
+       //coneccion por medio de HTTP
+       ConexionHTTP connet=new ConexionHTTP();
+       List<Cliente> clientes;
+       Type type=new TypeToken<List<Cliente>>(){}.getType();
+       clientes=connet.conectarServicio("http://localhost:8080/fourcapitalservice/webresources/entidades.cliente", "",type);
+       System.out.println(clientes.size());
+       
+       
+       // coneccion por medio de cliente del asistente netbeans
+//       List<Cliente> clientes=new ArrayList<>();
+//       ClienteREST cli=new ClienteREST(); 
+//       clientes=cli.findAll_JSON(clientes.getClass());
+//       System.out.println(clientes.size());
+   }
 }
